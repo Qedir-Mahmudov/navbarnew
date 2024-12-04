aa9c1b6dc2ce152846e9cc26ee5f9d48939143eb
huseyn <huseynea-bp216@code.edu.az>
2024-12-04 18:56:56 +0400

    bir

---
+++ script.js
@@ -0,0 +1,20 @@
+/* const em=document.querySelectorAll("#em"); */
+/* document.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+} */
+const em = document.querySelector("#em");
+
+
+  em.addEventListener("click", function() {
+   
+
+    em.style.backgroundColor = "lightblue";
+/*     document.getElementsByClassName(".logindiv").innerHTML = "Sign IN"; */ 
+    em.size=20;
+    alert("Emaili yaz");
+  });
+
+
+
